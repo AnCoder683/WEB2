0abf8c4de9cbf681b761bde400b4733932354c65
An <antran25722@gmail.com>
2024-02-27 15:22:18 +0700

    Add

---
+++ caculator.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.button-cacl').forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        let soA = parseFloat(document.querySelector('#soA').value);
+        let soB = parseFloat(document.querySelector('#soB').value);
+        let id = button.id
+        let ketQua = 0;
+        if(id === 'plus'){
+            ketQua = soA + soB;
+        }else if(id === 'decs'){
+            ketQua = soA - soB;
+        }else if(id === 'mul'){
+            ketQua = soA * soB;
+        }else if(id === 'div'){
+            ketQua = soA / soB;
+        }
+        document.querySelector('#ketQua').value = ketQua;
+    });
+});
